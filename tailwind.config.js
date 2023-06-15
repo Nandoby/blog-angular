1cea8d3e59e51a4d87cbf256a01f023e64c53022
Nandoby <nandobiaccalli@gmail.com>
2023-06-16 00:05:40 +0200

    Mise en place du routeur + HomePage

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,17 +7,26 @@ module.exports = {
     extend: {},
   },
   plugins: [require('daisyui')],
-  // daisyui: {
-  //   themes: [
-  //     {
-  //       mytheme: {
-  //         "primary": "#e11d48",
-  //         "secondary": "#4b5563",
-  //         "accent": "#CAFFD0",
-  //         "neutral": "#444"
-  //       }
-  //     }
-  //   ]
-  // }
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#e11d48",
+          "secondary": "#4b5563",
+          "accent": "#1dcdbc",
+          "neutral": "#2b3440",
+          "base-100": "#ffffff",
+          "info": "#3abff8",
+          "success": "#36d399",
+          "warning": "#fbbd23",
+          "error": "#f87272",
+        }
+      }
+    ],
+    darkTheme: "dark",
+    base: true,
+    styled: true,
+    utils: true,
+  }
 }
 
