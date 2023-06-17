b80901a3eec6ccc67dea12cbc2f32d6890f23d40
Nandoby <nandobiaccalli@gmail.com>
2023-06-17 13:12:44 +0200

    init users.service

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -11,8 +11,6 @@ export class UsersService {
 
   url: string = "http://localhost:3000/api/users"
 
-
-
   findAllUsers() {
     this.httpClient.get(this.url).subscribe({
       next: (value) => console.log(value)
