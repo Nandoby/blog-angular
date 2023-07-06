457b0a65a5cae0e883d7110eda2b6ff17dd65c1f
Nandoby <nandobiaccalli@gmail.com>
2023-07-06 22:47:29 +0200

    clean header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CategoriesService } from '../../shared/services/categories.service';
 import { Category } from '../../shared/interfaces/category.interface';
-import { ArticlesService } from '../../shared/services/articles.service';
 import { Router } from '@angular/router';
-import { AuthService } from '../../shared/services/auth.service';
-import { BehaviorSubject, Observable, Subscription, filter, first, tap } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { User } from '../../shared/interfaces/user.interface';
 import { Store } from '@ngrx/store';
-import { selectIsLoggedin, selectUser } from '../auth/shared/store/auth.selectors';
+import { selectUser } from '../auth/shared/store/auth.selectors';
 import { logoutAction } from '../auth/shared/store/auth.actions';
 
 @Component({
@@ -18,7 +16,6 @@ import { logoutAction } from '../auth/shared/store/auth.actions';
 export class HeaderComponent implements OnInit {
   constructor(
     private categoriesService: CategoriesService,
-    private articlesService: ArticlesService,
     private router: Router,
     private store: Store
   ) {}
